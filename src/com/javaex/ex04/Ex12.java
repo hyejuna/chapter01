9ba7a53b47514fdad3b24916dcc0988b7d220769
나혜주 <he_ju0609@naver.com>
2021-11-17 15:52:04 +0900

    do 반복문

---
+++ src/com/javaex/ex04/Ex12.java
@@ -0,0 +1,14 @@
+package com.javaex.ex04;
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+		int num=0;
+		do {
+			System.out.println("I like java"+num);
+			num++;
+		} while(num<5);
+
+	}
+
+}
