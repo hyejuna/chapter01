db748676eb062818255c1fe595bde09d1801aee2
나혜주 <he_ju0609@naver.com>
2021-11-17 12:24:20 +0900

    반복문

---
+++ src/com/javaex/ex04/Ex04.java
@@ -0,0 +1,19 @@
+package com.javaex.ex04;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		int n=0;
+		int i;
+		
+		for(i=1; i<=10; i++) {
+			
+			n=n+i;
+			System.out.println(i+" 까지의 합은 "+n);
+		}
+		System.out.println("1부터 "+ (i-1) +" 까지의 정수의 합은 "+n+" 입니다.");
+		
+
+	}
+
+}
---
+++ src/com/javaex/ex04/Ex05.java
@@ -0,0 +1,17 @@
+package com.javaex.ex04;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		int i,j;
+		
+		for(i=2; i<=9; i++) {
+			for(j=1; j<=9; j++) {
+				System.out.println(i+" * "+j+" = "+(i*j));
+			}
+			System.out.println("");
+		}
+		
+	}
+
+}
